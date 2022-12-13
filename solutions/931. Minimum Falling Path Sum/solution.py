8821afcfab3867f9362bcb7c8d98eb0fc1173d98
BulyginMaksim <max.buligin@gmail.com>
2022-12-13 10:39:11 +0300

    improved 931 problem to dp

--- solutions/931. Minimum Falling Path Sum/solution.py
+++ solutions/931. Minimum Falling Path Sum/solution.py
@@ -5,37 +5,13 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n_rows, n_cols = len(matrix), len(matrix[0])
         dp = [[0 for _ in range(n_cols)] for _ in range(n_rows)]
-        # iterate from bottom to top to not do overcalculations
-        for idx in range(n_rows - 1, -1, -1):
-            for jdx in range(n_cols - 1, -1, -1):
-                self.dfs(matrix, dp, idx, jdx)
+        # init with default state
+        for jdx in range(n_cols):
+            dp[n_rows - 1][jdx] = matrix[n_rows - 1][jdx]
+        for idx in range(n_rows - 2, -1, -1):
+            for jdx in range(n_cols):
+                left_most = dp[idx + 1][jdx - 1] if 0 <= jdx - 1 else float('inf')
+                mid_most = dp[idx + 1][jdx]
+                right_most = dp[idx + 1][jdx + 1] if jdx + 1 < n_cols else float('inf')
+                dp[idx][jdx] = matrix[idx][jdx] + min(left_most, mid_most, right_most)
         return min(dp[0])
-
-    def dfs(self, matrix: List[List[int]], dp: List[List[int]],
-            idx: int, jdx: int) -> int:
-        if dp[idx][jdx]:
-            return dp[idx][jdx]
-        # only special moves from the task
-        moves = (
-            (1, -1),
-            (1, 0),
-            (1, 1)
-        )
-        dp[idx][jdx] = matrix[idx][jdx]
-        min_path_found = None
-        for move in moves:
-            i, j = move
-            if not self.is_good_idx(idx + i, jdx + j, matrix):
-                continue
-            got_path = self.dfs(matrix, dp, idx + i, jdx + j)
-            if min_path_found is None or got_path < min_path_found:
-                min_path_found = got_path
-        if min_path_found is None:
-            min_path_found = 0
-        dp[idx][jdx] += min_path_found
-        return dp[idx][jdx]
-
-    def is_good_idx(self, idx: int, jdx: int, matrix: List[List[int]]) -> bool:
-        idx_is_good = 0 <= idx < len(matrix)
-        jdx_is_good = 0 <= jdx < len(matrix[0])
-        return idx_is_good and jdx_is_good
